aa02a350228427d847d1c3ed2b169e2b8490f692
Cédric Belin <cedric@belin.io>
2016-11-27 03:50:43 +0100

    Added the `Author` class and its unit tests

---
+++ test/AuthorTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Implementation of the `yii\akismet\test\AuthorTest` class.
+ */
+namespace yii\akismet\test;
+use akismet\{Author};
+
+/**
+ * Tests the features of the `yii\akismet\Author` class.
+ */
+class AuthorTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests the `Author` constructor.
+   */
+  public function testConstructor() {
+    $author = new Author([
+      'email' => 'cedric@belin.io',
+      'ipAddress' => '192.168.0.1',
+      'name' => 'Cédric Belin'
+    ]);
+
+    $this->assertEquals('cedric@belin.io', $author->getEmail());
+    $this->assertEquals('192.168.0.1', $author->getIPAddress());
+    $this->assertEquals('Cédric Belin', $author->getName());
+  }
+
+  /**
+   * Tests the `Author::jsonSerialize()` method.
+   */
+  public function testJsonSerialize() {
+    $data = (new Author())->jsonSerialize();
+    $this->assertEquals(0, count((array) $data));
+
+    $data = (new Author([
+      'email' => 'cedric@belin.io',
+      'ipAddress' => '127.0.0.1',
+      'name' => 'Cédric Belin',
+      'url' => 'https://belin.io'
+    ]))->jsonSerialize();
+
+    $this->assertEquals('Cédric Belin', $data->comment_author);
+    $this->assertEquals('cedric@belin.io', $data->comment_author_email);
+    $this->assertEquals('https://belin.io', $data->comment_author_url);
+    $this->assertEquals('127.0.0.1', $data->user_ip);
+  }
+}
