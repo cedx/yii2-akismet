27720e3600cc6eebb99c292e68cb31806472d7f5
Cédric Belin <cedric@belin.io>
2017-03-26 20:19:33 +0200

    Added the `CommentType` enumeration

---
+++ lib/CommentType.php
@@ -0,0 +1,23 @@
+<?php
+namespace yii\akismet;
+
+/**
+ * Specifies the type of a comment.
+ */
+final class CommentType {
+
+  /**
+   * @var string A standard comment.
+   */
+  const COMMENT = 'comment';
+
+  /**
+   * @var string A [pingback](https://en.wikipedia.org/wiki/Pingback) comment.
+   */
+  const PINGBACK = 'pingback';
+
+  /**
+   * @var string A [trackback](https://en.wikipedia.org/wiki/Trackback) comment.
+   */
+  const TRACKBACK = 'trackback';
+}
