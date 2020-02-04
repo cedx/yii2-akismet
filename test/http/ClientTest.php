93c573cfdbdafdb28324d0b2f9ca871c3d487327
Cédric Belin <cedric@belin.io>
2020-02-04 21:23:31 +0100

    Updated the user agent string

--- test/http/ClientTest.php
+++ test/http/ClientTest.php
@@ -75,7 +75,7 @@ class ClientTest extends TestCase {
       'isTest' => true
     ]);
 
-    $author = new Author('192.168.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0', [
+    $author = new Author('192.168.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 Edg/79.0.309.71', [
       'name' => 'Akismet',
       'role' => 'administrator',
       'url' => new Uri('https://dev.belin.io/yii2-akismet')
